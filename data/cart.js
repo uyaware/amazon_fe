cf9b1007d870a8b5829c15de86f8939bc3a6a41f
uyaware <tranuyqhc1@gmail.com>
2024-11-12 08:20:22 +0700

    Generate HTML in Checkout page

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,13 @@
-export const cart = [];
+export const cart = [
+  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 2,
+  },
+  {
+    id: "bc2847e9-5323-403f-b7cf-57fde044a1231445",
+    quantity: 1,
+  }
+];
 
 export function addProductToCart(productId, quantity) {
   let matchingItem;
