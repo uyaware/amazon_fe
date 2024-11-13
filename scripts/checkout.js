0731b9f1327368b1a72df119fc00b5cde7fdae2b
uyaware <tranuyqhc1@gmail.com>
2024-11-13 19:36:39 +0700

    convert loadProduct from XMLHttpRequest to Fetch

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,16 +1,12 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadCart } from "../data/cart.js";
-import { loadProducts } from "../data/products.js";
+import { loadProducts, loadProductsFetch } from "../data/products.js";
 //import '../data/cart-oop.js'
 //import '../data/backend-practice.js';
 
 Promise.all([
-  new Promise((resolve) => {
-    loadProducts(() => {
-      resolve();
-    });
-  }),
+  loadProductsFetch(),
   new Promise((resolve) => {
     loadCart(() => {
       resolve();
